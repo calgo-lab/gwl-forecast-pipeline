1c8263466a3638f2fa4432ced1a67c6dc3faa35f
Alexander Schulz <info@alexander-schulz.eu>
2023-03-03 14:38:29 +0100

    update project

--- src/gwl_forecast_pipeline/config.py
+++ src/gwl_forecast_pipeline/config.py
@@ -95,7 +95,7 @@ else:
     root = logging.getLogger('gwl_forecast_pipeline')
     root.setLevel(logging.DEBUG)
     handler = logging.StreamHandler(sys.stdout)
-    handler.setLevel(logging.DEBUG)
+    handler.setLevel(logging.INFO)
     formatter = logging.Formatter(
         '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
